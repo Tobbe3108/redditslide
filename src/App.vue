1510f427b046419f3493bca180168ff855c54dc7
Tobias <Tobbe3108@gmail.com>
2019-11-12 15:00:16 +0100

    Large Changes

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       :limit="limit"
       :after="after"
       :results="results"
-      @updateSubreddit="updateSubreddit"
+      @nextPage="nextPage"
     />
   </div>
 </template>
@@ -15,6 +15,7 @@
 <script>
 import RedditSlide from "./components/RedditSlide.vue";
 import axios from "axios";
+import { isNullOrUndefined } from "util";
 
 export default {
   name: "app",
@@ -38,23 +39,47 @@ export default {
     this.urlObject = new URL(this.url);
     this.subreddit = this.urlObject.pathname.split("/")[2];
     this.sort = this.urlObject.pathname.split("/")[3];
+    if (this.sort === "") {
+      this.sort = `hot`;
+    }
     this.period = this.urlObject.searchParams.get("t");
     this.limit = this.urlObject.searchParams.get("limit");
     this.after = this.urlObject.searchParams.get("after");
-    this.fetchPosts(
-      this.generateURL(
-        this.subreddit,
-        this.sort,
-        this.period,
-        this.limit,
-        this.after
-      )
-    );
+    this.fetchPosts();
+  },
+  computed: {
+    generateURL: function() {
+      //https://www.reddit.com/r/${subreddit}/${sort}.json?&t=${period}&limit=${limit}&after=${after}
+
+      let url = "https://www.reddit.com/r";
+
+      if (!isNullOrUndefined(this.subreddit)) {
+        url += `/${this.subreddit}`;
+      }
+
+      if (!isNullOrUndefined(this.sort)) {
+        url += `/${this.sort}` + ".json?";
+      }
+
+      if (!isNullOrUndefined(this.period)) {
+        url += `&t=${this.period}`;
+      }
+
+      if (!isNullOrUndefined(this.limit)) {
+        url += `&limit=${this.limit}`;
+      }
+
+      if (!isNullOrUndefined(this.after)) {
+        url += `&after=${this.after}`;
+      }
+
+      return url;
+    }
   },
   methods: {
-    fetchPosts: async function(url) {
+    fetchPosts: async function() {
       const res = await axios
-        .get(url)
+        .get(this.generateURL)
         .then(res => (res = res.data.data.children));
       let data = res.map(posts => posts.data);
       data = data.filter(
@@ -65,42 +90,9 @@ export default {
       );
       this.results = data;
     },
-    generateURL: function(subreddit, sort, period, limit, after) {
-      //https://www.reddit.com/r/${subreddit}/${sort}.json?&t=${period}&limit=${limit}&after=${after}
-
-      let url = "https://www.reddit.com/r";
-
-      if (subreddit !== null) {
-        url += `/${subreddit}`;
-      }
-
-      if (sort === undefined || sort === null) {
-        this.sort = "hot";
-        url += `/hot` + ".json?";
-      } else {
-        url += `/${sort}` + ".json?";
-      }
-
-      if (period !== null || period !== undefined) {
-        url += `&t=${period}`;
-      }
-
-      if (limit !== null || limit !== undefined) {
-        url += `&limit=${limit}`;
-      }
-
-      if (after !== null || after !== undefined) {
-        url += `&after=${after}`;
-      }
-
-      return url;
-    },
-    updateSubreddit: function(subreddit, sort, period, limit, after) {
-      this.subreddit = subreddit;
-      this.sort = sort;
-      this.period = period;
-      this.limit = limit;
+    nextPage: function(after) {
       this.after = after;
+      this.fetchPosts();
     }
   }
 };
--- src/components/RedditSlide.vue
+++ src/components/RedditSlide.vue
@@ -1,35 +1,64 @@
 <template>
   <div id="app">
-    <p>Subreddit: {{ subreddit }}</p>
-    <p>Sort: {{ sort }}</p>
-    <p>Period: {{ period }}</p>
-
     <input type="text" v-model="subreddit" />
-    <input type="text" v-model="sort" />
-    <input type="text" v-model="period" />
-
-    <input
-      type="submit"
-      @click="
-        $emit('updateSubreddit', subreddit, sort, period, limit, after, results)
-      "
-    />
+    <select @change="updatePeriod" v-model="sort">
+      <option value="hot">Hot</option>
+      <option value="new">New</option>
+      <option value="controversial">Controversial</option>
+      <option value="top">Top</option>
+      <option value="rising">Rising</option>
+    </select>
+    <select v-if="sort === 'top' || sort === 'controversial'" v-model="period">
+      <option value="hour">Hour</option>
+      <option value="day">Day</option>
+      <option value="week">Week</option>
+      <option value="month">Month</option>
+      <option value="year">Year</option>
+      <option value="all">All</option>
+    </select>
+    <a :href="URL">Go to subreddit</a>
 
     <div v-if="results">
-      <img
-        v-for="post in results"
-        :key="post.name"
-        :src="post.url"
-        :alt="post.name"
-      />
+      <img v-for="post in results" :key="post.name" :src="post.url" :alt="post.name" />
     </div>
+    <button @click="$emit('nextPage', results[results.length - 1].name)">Next</button>
   </div>
 </template>
 
 <script>
+import { isNullOrUndefined } from "util";
+
 export default {
   name: "RedditSlide",
-  props: ["subreddit", "sort", "period", "limit", "after", "results"]
+  props: ["subreddit", "sort", "period", "limit", "after", "results"],
+  computed: {
+    URL: function() {
+      let url = "http://localhost:8080/r";
+
+      if (!isNullOrUndefined(this.subreddit)) {
+        url += `/${this.subreddit}`;
+      }
+
+      if (!isNullOrUndefined(this.sort)) {
+        url += `/${this.sort}` + "?";
+      }
+
+      if (!isNullOrUndefined(this.period)) {
+        url += `&t=${this.period}`;
+      }
+
+      return url;
+    }
+  },
+  methods: {
+    updatePeriod: function() {
+      if (this.sort === "top" || this.sort === "controversial") {
+        this.period = "all";
+      } else {
+        this.period = null;
+      }
+    }
+  }
 };
 </script>
 
