abe2c3f7e9358dd6c1c4d90209cecb5fac3d86ea
Tobbe3108 <Tobbe3108@gmail.com>
2019-11-12 01:05:55 +0100

    Init Port of svelte app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RedditSlide
+      :subreddit="subreddit"
+      :sort="sort"
+      :period="period"
+      :limit="limit"
+      :after="after"
+      :results="results"
+      @updateSubreddit="updateSubreddit"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RedditSlide from "./components/RedditSlide.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    RedditSlide
+  },
+  data() {
+    return {
+      url: "",
+      urlObject: "",
+      subreddit: "",
+      sort: "",
+      period: "",
+      limit: "",
+      after: "",
+      results: ""
+    };
+  },
+  created: function() {
+    this.url = window.location;
+    this.urlObject = new URL(this.url);
+    this.subreddit = this.urlObject.pathname.split("/")[2];
+    this.sort = this.urlObject.pathname.split("/")[3];
+    this.period = this.urlObject.searchParams.get("t");
+    this.limit = this.urlObject.searchParams.get("limit");
+    this.after = this.urlObject.searchParams.get("after");
+    this.fetchPosts(
+      this.generateURL(
+        this.subreddit,
+        this.sort,
+        this.period,
+        this.limit,
+        this.after
+      )
+    );
+  },
+  methods: {
+    fetchPosts: async function(url) {
+      const res = await axios
+        .get(url)
+        .then(res => (res = res.data.data.children));
+      let data = res.map(posts => posts.data);
+      data = data.filter(
+        it => it.domain == "i.imgur.com" || it.domain == "i.redd.it" //||
+        //it.domain == "youtu.be" ||
+        //it.domain == "imgur.com" ||
+        //it.domain == "www.youtube.com"
+      );
+      this.results = data;
+    },
+    generateURL: function(subreddit, sort, period, limit, after) {
+      //https://www.reddit.com/r/${subreddit}/${sort}.json?&t=${period}&limit=${limit}&after=${after}
+
+      let url = "https://www.reddit.com/r";
+
+      if (subreddit !== null) {
+        url += `/${subreddit}`;
+      }
+
+      if (sort === undefined || sort === null) {
+        this.sort = "hot";
+        url += `/hot` + ".json?";
+      } else {
+        url += `/${sort}` + ".json?";
+      }
+
+      if (period !== null || period !== undefined) {
+        url += `&t=${period}`;
+      }
+
+      if (limit !== null || limit !== undefined) {
+        url += `&limit=${limit}`;
+      }
+
+      if (after !== null || after !== undefined) {
+        url += `&after=${after}`;
+      }
+
+      return url;
+    },
+    updateSubreddit: function(subreddit, sort, period, limit, after) {
+      this.subreddit = subreddit;
+      this.sort = sort;
+      this.period = period;
+      this.limit = limit;
+      this.after = after;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/RedditSlide.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <p>Subreddit: {{ subreddit }}</p>
+    <p>Sort: {{ sort }}</p>
+    <p>Period: {{ period }}</p>
+
+    <input type="text" v-model="subreddit" />
+    <input type="text" v-model="sort" />
+    <input type="text" v-model="period" />
+
+    <input
+      type="submit"
+      @click="
+        $emit('updateSubreddit', subreddit, sort, period, limit, after, results)
+      "
+    />
+
+    <div v-if="results">
+      <img
+        v-for="post in results"
+        :key="post.name"
+        :src="post.url"
+        :alt="post.name"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RedditSlide",
+  props: ["subreddit", "sort", "period", "limit", "after", "results"]
+};
+</script>
+
+<style></style>
